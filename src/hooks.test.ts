0a9e28100f4bfef6c3569ac2f222ff97944a2321
Timo Bechtel <hi@timobechtel.com>
2022-03-18 17:07:31 +0100

    test: add a few more tests

--- src/hooks.test.ts
+++ src/hooks.test.ts
@@ -55,3 +55,63 @@ test('allows manipulating arguments', async () => {
 	const result = await hooks.call('my-hook', { args: testArgsInput });
 	expect(result).toBe(testArgsOutput);
 });
+
+test('arguments are cloned by default', async () => {
+	type Hooks = {
+		'test:hook': Hook<{ data: { message: string } }>;
+	};
+	const hooks = createHooks<Hooks>();
+	const exampleData = { data: { message: 'hello world' } };
+
+	hooks.register('test:hook', ({ data }) => {
+		expect(data.message).toEqual('hello world');
+		expect(data).not.toBe(exampleData.data);
+	});
+
+	await hooks.call('test:hook', { args: exampleData });
+});
+
+test('allow cancelling hook with error message', (done) => {
+	type Hooks = {
+		'test:hook': Hook<{ message: string }>;
+	};
+	const hooks = createHooks<Hooks>();
+	let hookTriggered = false;
+
+	hooks.register('test:hook', ({ message }) => {
+		expect(message).toEqual('hello world');
+		throw new Error('hook failed');
+	});
+	hooks
+		.call('test:hook', { args: { message: 'hello world' } })
+		.then(() => {
+			hookTriggered = true;
+		})
+		.catch(({ message }) => {
+			expect(message).toEqual('hook failed');
+		});
+
+	setTimeout(() => {
+		expect(hookTriggered).toBe(false);
+		done();
+	}, 10);
+});
+
+test('allow asynchronous functions as middleware', (done) => {
+	type Hooks = {
+		'test:hook': Hook<{ data: any }>;
+	};
+	const hooks = createHooks<Hooks>();
+
+	hooks.register('test:hook', async (data) => {
+		expect(data).toEqual(undefined);
+		return {
+			data: 'hello world',
+		};
+	});
+	hooks.register('test:hook', (data) => {
+		expect(data).toEqual({ data: 'hello world' });
+		done();
+	});
+	hooks.call('test:hook');
+});
